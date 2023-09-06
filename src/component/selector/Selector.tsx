d5f69cc21b023b8fbf45d198e2b028dab10c8ce1
Maru5er <74164821+Maru5er@users.noreply.github.com>
2023-09-06 21:59:29 +0700

    table fixed and added clickable item

--- src/component/selector/Selector.tsx
+++ src/component/selector/Selector.tsx
@@ -1,25 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { setParameter, setValue } from "./selectorSlice";
+import Table from "react-bootstrap/Table";
+import { ITool } from "./toolInterface";
 
 const Selector : React.FC = () => {
     const dispatch = useAppDispatch();
     const parameter : string = useAppSelector((state) => state.selector.parameter);
     const value : string = useAppSelector((state) => state.selector.value);
-    const [data, setData] = useState<JSX.Element[]>([]);
+    const [data, setData] = useState<ITool[]>([]);
+    const [selectedItemID, setSelectedItemID] = useState<string>("");
     const url = process.env.serverURL
     async function generateTable() {
-        let dataPromise = fetch("http://localhost:4567"+ `/tools/${parameter}&${value}`)
+        let dataPromise = fetch("http://localhost:4567/" + `tools/${parameter}&${value}`)
         .then(response => response.json())
         .then((parsedData) => {
-            const toolData = parsedData.tools;
+            const toolData = parsedData.tool;
             console.log(toolData);
-            for (let i : number = 0; i < toolData.length; i++) {
-                let item = <div>{toolData[i]}</div>
-                setData((prevData) => [...prevData, toolData[i]]);
-            }
+            setData(toolData);
         }).catch((error) => {alert(error)});
     }
+
+    
+    useEffect(() => {
+        async function getAllData() {
+            let dataPromise = fetch("http://localhost:4567/tools")
+            .then(response => response.json())
+            .then((parsedData) => {
+                const toolData = parsedData.tools;
+                setData(toolData);
+            }).catch((error) => {alert(error)});
+        }
+        getAllData();
+    },[]);
+
+
+
 
     return (
         <div>
@@ -48,7 +64,35 @@ const Selector : React.FC = () => {
                     }
                 }>search</button>
             </div>
-            {data}
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>name</th>
+                        <th>code</th>
+                        <th>diameter</th>
+                        <th>size</th>
+                        <th>angle</th>
+                        <th>Date in</th>
+                        <th>Date out</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data && data.map((item) => (
+                        <tr onClick= {() => setSelectedItemID(item._id)}>
+                            <td>{item.name}</td>
+                            <td>{item.code}</td>
+                            <td>{item.diameter}</td>
+                            <td>{item.size}</td>
+                            <td>{item.angle}</td>
+                            <td>{item.dateIn && item.dateIn.toString()}</td>
+                            <td>{item.dateOut && item.dateOut.toString()}</td>
+                            <td>{item.status && item.status}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+            selected item id: {selectedItemID}
         </div>
     )
 }
---
+++ src/component/selector/toolInterface.ts
@@ -0,0 +1,11 @@
+export interface ITool {
+    _id : string,
+    name : string, 
+    code : string, 
+    angle : number,
+    diameter : number,
+    size : number,
+    dateIn : Date,
+    dateOut : Date,
+    status : string,
+}
