6a487bdb7da4008d50508655d9fc1f8c745c56e5
Marco Hendra Dani <marcohendradani@gmail.com>
2023-09-14 15:05:19 +0700

    table css

--- src/component/selector/Selector.tsx
+++ src/component/selector/Selector.tsx
@@ -243,7 +243,7 @@ const Selector : React.FC = () => {
                 }>search</button>
                 <button className="delete-button" onClick={() => deleteTool()}>delete</button>
             </div>
-            
+
             <h3>Update Item</h3>
             <div id="update-parameter">
                 <input type="text" name = "parameter" placeholder="parameter"
@@ -253,48 +253,51 @@ const Selector : React.FC = () => {
                     updateTool();
                 }}>update</button>
             </div>
-            <Table bordered = {true}>
-                <thead className="table-border">
-                    <tr>
-                        <th>name</th>
-                        <th>code</th>
-                        <th>diameter</th>
-                        <th>size</th>
-                        <th>angle</th>
-                        <th>Material</th>
-                        <th>Height</th>
-                        <th>Status</th>
-                        <th>Machine</th>
-                        <th>Date in</th>
-                        <th>Date out</th>
-                        <th>Description</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data && data.map((item) => (
-                        <tr
-                            id={activeRow.includes(item._id) ? "selected" : "not-selected"}
-                            onClick= {() =>  {
-                                            setSelectedItemID([item._id]);
-                                            toggleActive(item._id);
-                                            }}
-                            key={item._id}>
-                            <td >{item.name}</td>
-                            <td >{item.code}</td>
-                            <td >{item.diameter}</td>
-                            <td >{item.size}</td>
-                            <td >{item.angle}</td>
-                            <td >{item.material && item.material}</td>
-                            <td >{item.height && item.height}</td>
-                            <td >{item.status && item.status}</td>
-                            <td >{item.machine && item.machine}</td>
-                            <td >{item.dateIn && item.dateIn.toString()}</td>
-                            <td >{item.dateOut && item.dateOut.toString()}</td>
-                            <td >{item.description && item.description}</td>
+            <div className="table-container">
+                <Table bordered = {true}>
+                    <thead className="table-border">
+                        <tr>
+                            <th>name</th>
+                            <th>code</th>
+                            <th>diameter</th>
+                            <th>size</th>
+                            <th>angle</th>
+                            <th>Material</th>
+                            <th>Height</th>
+                            <th>Status</th>
+                            <th>Machine</th>
+                            <th>Date in</th>
+                            <th>Date out</th>
+                            <th>Description</th>
                         </tr>
-                    ))}
-                </tbody>
-            </Table>
+                    </thead>
+                    <tbody>
+                        {data && data.map((item) => (
+                            <tr
+                                id={activeRow.includes(item._id) ? "selected" : "not-selected"}
+                                onClick= {() =>  {
+                                                setSelectedItemID([item._id]);
+                                                toggleActive(item._id);
+                                                }}
+                                key={item._id}>
+                                <td >{item.name}</td>
+                                <td >{item.code}</td>
+                                <td >{item.diameter}</td>
+                                <td >{item.size}</td>
+                                <td >{item.angle}</td>
+                                <td >{item.material && item.material}</td>
+                                <td >{item.height && item.height}</td>
+                                <td >{item.status && item.status}</td>
+                                <td >{item.machine && item.machine}</td>
+                                <td >{item.dateIn && item.dateIn.toString()}</td>
+                                <td >{item.dateOut && item.dateOut.toString()}</td>
+                                <td >{item.description && item.description}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </div>
+            
         </div>
     )
 }
